91b20c6b54ab49e0ccb9ab4fc130094018441e32
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2024-01-14 17:19:50 +0100

    Added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/caroline-flora-knight-48a24b118">
+            Caroline Knight
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a href="https://github.com/carolineflora95/react-dictionary-app">
+            Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
